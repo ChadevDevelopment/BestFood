85476bfad06b12af54f4b7814290cee793117c2a
mehmetkaynar <mehmetkynrr34@gmail.com>
2024-06-11 18:37:51 +0200

    footer and header added

---
+++ best_food_next/components/navbar/Hero.tsx
@@ -0,0 +1,12 @@
+import Navbar from "./Navbar";
+
+const Hero = () => {
+  return (
+    <div className="relative min-h-screen ">
+      <Navbar />
+      <hr />
+    </div>
+  );
+};
+
+export default Hero;
